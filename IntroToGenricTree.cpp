3c02bcb39ec2d0f114ed4305baa649789869b541
Nikhil-69 <nikhil.mittal.ug20@nsut.ac.in>
2021-07-14 03:46:23 +0530

    Trees

--- IntroToGenricTree.cpp
+++ IntroToGenricTree.cpp
@@ -23,6 +23,23 @@ void printTree(node* root)
         printTree(root->children[i]);
     }
 }
+void printTreeLevelWise(node* root)
+{
+    queue<node*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size()!=0)
+    {
+        node* front=pendingNodes.front();
+        pendingNodes.pop();
+        cout<<front->data<<" : ";
+        for(int i=0;i<front->children.size();i++)
+        {
+             pendingNodes.push(front->children[i]);
+             cout<<front->children[i]->data<<" , ";
+        }
+        cout<<endl;
+    }
+}
 node* takeInput(){
     cout<<"Enter data\t";
     int data;cin>>data;
@@ -36,10 +53,34 @@ node* takeInput(){
     }
     return root;
 };
+node* takeInputLevelWise(){
+    cout<<"Enter Root"<<":\t";;
+    int data;cin>>data;
+    node* root=new node(data);
+    queue<node*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size()!=0)
+    {
+        node* front=pendingNodes.front();
+        pendingNodes.pop();
+        cout<<"\nEnter Number of child's of "<<front->data<<":\t";
+        int childNum;cin>>childNum;
+        for(int i=0;i<childNum;i++)
+        {
+            int childData;
+            cout<<"\nEnter "<<i<<"th child of "<<front->data<<":\t";;
+            cin>>childData;
+            node* child = new node(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
 int main(){
     
-   node* root= takeInput();
-    printTree(root);
+    node* root= takeInputLevelWise();
+    printTreeLevelWise(root);
     
 return 0;
 }
