3ae5abc833dfa5a4778814c64e2a8f782c5212c1
Nikhil-69 <nikhil.mittal.ug20@nsut.ac.in>
2021-07-26 22:03:06 +0530

    Revise

--- LinkedLists/Introduction_to_LInked_List.cpp
+++ LinkedLists/Introduction_to_LInked_List.cpp
@@ -10,76 +10,80 @@ public:
         data=x;
         next=NULL;
     }
-    
 };
-
 int search(node* &head,int x)
 {
-    int pos=0;
     node* current=head;
-    while(current!=NULL)
+    for(int pos=0;current!=NULL;pos++)
     {
         if(current->data==x)
         {
             return pos;
         }
-        pos++;
+        
         current=current->next;
     }
     return -1;
 }
-
 void addhead(node* &head,int data)
 {
-    node* n=new node(data);
+    node* n=new node(data);//new node
     n->next=head;
     head=n;
 }
-
 void pushback(node* &head,int data)
 {
     node* n=new node(data);
-    if(head==NULL)
+    if(head==NULL)  //base
     {
         head=n;
         return;
     }
-    node* current=head;
+    node* current=head;     //for safe side 
     
-    while(current->next != NULL)
+    while(current->next != NULL) //traverse to last
     {
         current=current->next;
     }
-    current->next=n;
+    current->next=n;//put last->next= Jo new node bnai thi
 }
-
-
 void display(node* &head)
 {
-    node* current=head;
-    while(current!=NULL)
+    node* current=head; //safe side
+    while(current!=NULL)        //travese
     {
-        cout<<current->data<<"\t";;
+        cout<<current->data<<"\t";  //printing
         current=current->next;
     }
     cout<<endl;
 }
-
+void addAtIdx(node* &head,int data,int idx)
+{
+    int count=0;
+    node* n=new node(data);
+    node* current=head;
+    while(current->next!=NULL &&  count<idx-1)
+    {
+                current=current->next;
+                count++;
+    }
+    node* temp=current->next;
+    current->next=n;
+    n->next=temp;
+}
 
 int main()
 {
-
-    node *LL1=NULL;
-    addhead(LL1,0);
-    pushback(LL1,10);
-    pushback(LL1,20);
-    pushback(LL1,50);
-    pushback(LL1,90);
-    
+    node* LL1=NULL;
+    pushback(LL1,0);
+    pushback(LL1,1);
+    pushback(LL1,2);
+    pushback(LL1,4);
+    pushback(LL1,5);
+    pushback(LL1,6);
+    addAtIdx(LL1,69,3);
+    cout<<"\n"<<search(LL1,6969)<<endl;
     display(LL1);
-    cout<<search(LL1, 50)<<endl;
-    
-    
-    
+
 return 0;
 }
